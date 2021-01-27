c96e5a862a70ff1e5f432c15c6c77a4e283b286e
Boyd Smit <boydsmit@live.nl>
2021-01-27 11:24:14 +0100

    Added Header file (Calculator.hpp) for Calculator

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,45 +1,43 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include "Calculator.hpp"
 
 int sub1;
 int sub2;
-string equation;
-
-class func {
-public:
-	static void parseStringToInt(char ope);
-};
 
 int main()
 {
-	cout << "Please Enter a sum:\n";
-	cin >> equation;
+	std::string equation;
+
+	std::cout << "Please Enter a sum:\n";
+	std::cin >> equation;
 
 	if (equation.find('+') != std::string::npos) {
-		func::parseStringToInt('+');
-		cout << "The answer is: " << sub1 + sub2;
+		parseStringToInt(equation, '+');
+		std::cout << "The answer is: " << sub1 + sub2;
 	}
 	else if(equation.find('-') != std::string::npos){
-		func::parseStringToInt('-');
-		cout << "The answer is: " << sub1 - sub2;
+		parseStringToInt(equation, '-');
+		std::cout << "The answer is: " << sub1 - sub2;
 	}
 	else if (equation.find('*') != std::string::npos) {	
-		func::parseStringToInt('*');
-		cout << "The answer is: " << sub1 * sub2;
+		parseStringToInt(equation, '*');
+		std::cout << "The answer is: " << sub1 * sub2;
 	}
 	else if (equation.find('/') != std::string::npos) {
-		func::parseStringToInt('/');
-		cout << "The answer is: " << sub1 / sub2;
+		parseStringToInt(equation, '/');
+		std::cout << "The answer is: " << sub1 / sub2;
 	}
 	else {
-		cout << "Sum did not contain a valid operator.";
+		std::cout << "Sum did not contain a valid operator.";
 		return 0;
 	}
 }
 
-void func::parseStringToInt(char ope){
-	sub1 = stoi(equation.substr(0, equation.find(ope)));
-	sub2 = stoi(equation.substr(equation.find(ope), string::npos));
+void parseStringToInt(std::string equation,char ope){
+	try {
+		sub1 = std::stoi(equation.substr(0, equation.find(ope)));
+		sub2 = std::stoi(equation.substr(equation.find(ope), std::string::npos));
+	} catch (std::exception &err) {
+		std::cout << "Something went wrong parsing the string to an integer";
+	}
 }
 
